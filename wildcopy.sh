255fbb443cf0c14f4b9564faf173143aab597e35
robinsmith3 <robinsmith3@gmail.com>
2025-03-18 10:58:16 +0000

    Please enter the commit message for your changes. Lines starting

--- wildcopy.sh
+++ wildcopy.sh
@@ -1,4 +1,4 @@
-tar -cvf temp.tar *
+tar -cvf temp.tar frr.conf.$1 reload.sh reload-test.sh
 docker cp temp.tar $1:/etc/frr
 docker exec $1 tar -xvf /etc/frr/temp.tar -C /etc/frr
 docker exec $1 rm /etc/frr/temp.tar
